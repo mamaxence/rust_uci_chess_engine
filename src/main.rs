d43922142f55566f590496156bd10b00593fedff
menagerm <maxence.menager@grenoble-inp.org>
2021-04-27 22:45:48 +0200

    Add move uci parsing

--- src/main.rs
+++ src/main.rs
@@ -18,11 +18,13 @@ mod uci_parser {
             eprintln!("Empty input!")
         }
         eprintln!("got: {:?}", split_line);
+        let mut board = Board::new_empty_board();
         match split_line[0] {
             "uci" => parse_uci(),
             "isready" => parse_isready(),
             "ucinewgame" => parse_ucinewgame(),
-            "position" => parse_position(&split_line[1..]),
+            "position" => parse_position(&split_line[1..], &mut board),
+            "go" => println!("bestmove a7a5"),
             _ => eprintln!("Unsuported opperation : {}", line)
         }
     }
@@ -41,12 +43,18 @@ mod uci_parser {
 
     use crate::engine::board::Board;
 
-    pub fn parse_position(details: &[&str]){
-        let board:Board = match details[0]{
-            "startpos"=> Board::new_board(),
-            fen=> Board::new_from_fen(fen),
-        };
-        eprintln!("created board:\n{}", board)
+    pub fn parse_position(details: &[&str], board: &mut Board){
+        match details[0]{
+            "startpos"=> *board = Board::new_board(),
+            fen=> *board = Board::new_from_fen(fen),
+        }
+        if details.len() >= 1{
+            for mv in details[2..details.len()].to_vec(){
+                println!("mv: {}", mv);
+                *board = board.apply_move(&mv.parse().unwrap());
+            }
+        }
+        println!("new mv \n{}", board);
+        println!("fen: {}", board.to_fen());
     }
-
 }
